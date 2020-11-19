be3ebcbc25c1a265feb7c45064dce9e2e89941c5
Evan Stewart <estew012@sledge.cs.ucr.edu>
2020-11-18 23:45:52 -0800

    Create starvation test (unimplemented)

--- lab2.c
+++ lab2.c
@@ -5,15 +5,20 @@ int main(int argc, char *argv[])
 {
 	
 	int PScheduler(void);
+    int starvationTest(void);
 
-  printf(1, "\n This program tests the correctness of your lab#2\n");
-  
+    if (atoi(argv[1]) == 1)
+        PScheduler();
+    else if (atoi(argv[1]) == 2)
+        starvationTest();
+
 	PScheduler();
 	return 0;
 }
   
     
 int PScheduler(void){
+    printf(1, "\n This program tests the correctness of your lab#2\n");
     // use this part to test the priority scheduler. Assuming that the priorities range between range between 0 to 31
     // 0 is the highest priority and 31 is the lowest priority.
     int pid;
@@ -25,23 +30,21 @@ int PScheduler(void){
     printf(1, "\n  Step 2: The parent processes will switch to priority 0\n");
     changePriority(0);
     for (i = 0; i <  3; i++) {
-	pid = fork();
-	if (pid > 0 ) {
-		continue;}
-	else if ( pid == 0) {
-
-		changePriority(30-10*i);	
-		for (j=0;j<50000;j++) {
-			for(k=0;k<1000;k++) {
-				asm("nop"); 
+	    pid = fork();
+	    if (pid > 0 ) {
+		    continue;}
+	    else if ( pid == 0) {
+		    changePriority(30-10*i);	
+		    for (j=0;j<50000;j++) {
+			    for(k=0;k<1000;k++) {
+			    	asm("nop"); 
+                }
             }
-        }
 		printf(1, "\n child# %d with priority %d has finished! \n",getpid(),30-10*i);		
 		exit();
         }
         else {
 			printf(2," \n Error \n");
-			
         }
 	}
 
@@ -52,5 +55,36 @@ int PScheduler(void){
         printf(1,"\n if processes with highest priority finished first then its correct \n");
     }
 	exit();		
+    return 0;
+}
+
+int starvationTest(){
+    int pid;
+    int i,j,k;
+    int numprocesses = 8;
+    for (i = 0; i <  numprocesses; i++){
+	    pid = fork();
+	    if (pid > 0 ) {
+		    continue;}
+	    else if ( pid == 0) {
+		    changePriority(30-10*i);	
+		    for (j=0;j<50000;j++) {
+			    for(k=0;k<1000;k++) {
+			    	asm("nop"); 
+                }
+            }
+		printf(1, "\n child# %d with priority %d has finished! \n",getpid(),30-10*i);		
+		exit();
+        }
+        else {
+			printf(2," \n Error \n");
+        }
+	}
+    if(pid > 0) {
+		for (i = 0; i <  numprocesses; i++){
+			wait();
+		}
+        printf(1,"\n if processes with highest priority finished first then its correct \n");
+    }
     return 0;
 }
