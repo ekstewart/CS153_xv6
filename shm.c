8b6dc59cbea33ecbb040ba0b95b39090aa5900b7
Hoda Farhat <hfarh002@sledge.cs.ucr.edu>
2020-12-17 22:54:17 -0800

    lab 4 completed

--- shm.c
+++ shm.c
@@ -29,21 +29,65 @@ void shminit() {
 }
 
 int shm_open(int id, char **pointer) {
-
 //you write this
-
-
-
-
-return 0; //added to remove compiler warning -- you should decide what to return
+//grab the lock while you are working with the shm_table 
+  acquire(&(shm_table.lock));
+  int exists = 0;
+//empty entry in the shm_table
+  int entry = -1;
+  struct proc* h = myproc(); 
+  //In either case, return the virtual address through the second parameter of the system call.
+  uint va = PGROUNDUP(h->sz);
+  *pointer = (char*) va;  int i;
+  for (i = 0; i < 64; i++) {
+    //check segment id
+    //shm_open looks through the shm_table to see if this segment id already exists
+    if (shm_table.shm_pages[i].id == id) {
+      exists = 1;
+      break;
+    }
+    entry = i;
+  }
+  if (exists == 0) {
+    shm_table.shm_pages[entry].id = id; //initialize id to id passed to us
+    shm_table.shm_pages[entry].frame = kalloc(); //store its address in frame (we got our physical page)
+    shm_table.shm_pages[entry].refcnt = 1;
+    //store its address in frame (we got our physical page)
+    memset(shm_table.shm_pages[entry].frame, 0, PGSIZE); 
+    mappages(h->pgdir,(void*)va , PGSIZE, V2P(shm_table.shm_pages[entry].frame),PTE_U|PTE_W);
+  
+  }
+  //In either case, return the virtual address through the second parameter of the system call.
+  // uint va = PGROUNDUP(h->sz);
+  // *pointer = (char*) va;
+  else {
+    mappages(h->pgdir,(void*)va , PGSIZE, V2P(shm_table.shm_pages[i].frame),PTE_U|PTE_W);
+  }
+  shm_table.shm_pages[i].refcnt++; //we set the refcnt to 1
+  release(&(shm_table.lock));
+ 
+  return va; //added to remove compiler warning -- you should decide what to return
 }
 
 
 int shm_close(int id) {
 //you write this too!
-
-
-
-
-return 0; //added to remove compiler warning -- you should decide what to return
+  acquire(&(shm_table.lock));
+  int i;
+  int exists = 0;
+  for (i = 0; i < 64; i++) {
+    if (shm_table.shm_pages[i].id == id) {
+      exists = 1;
+      break;
+    }
+  }
+  if (exists == 1) {
+    if (shm_table.shm_pages[i].refcnt-- == 0) {
+      shm_table.shm_pages[i].id = 0;
+      shm_table.shm_pages[i].frame = 0;
+    } 
+  }
+  
+  release(&(shm_table.lock));
+  return 0; //added to remove compiler warning -- you should decide what to return
 }
